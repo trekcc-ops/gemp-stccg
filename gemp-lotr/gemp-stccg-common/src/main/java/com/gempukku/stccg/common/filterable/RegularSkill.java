b85d251114632f182a0b178d5fdc0e5ad52ff10f
Christopher Lund <lund_christopher@outlook.com>
2023-10-14 09:22:07 -0500

    Added some Delta Quadrant and Neutral Zone missions to further test spaceline creation methods. Also added enums for 1E regular skills and ship special equipment.

---
+++ gemp-lotr/gemp-stccg-common/src/main/java/com/gempukku/stccg/common/filterable/RegularSkill.java
@@ -0,0 +1,45 @@
+package com.gempukku.stccg.common.filterable;
+
+public enum RegularSkill implements Filterable {
+    // Classifications
+    ANIMAL("ANIMAL"), CIVILIAN("CIVILIAN"), ENGINEER("ENGINEER"),
+    MEDICAL("MEDICAL"), OFFICER("OFFICER"), SCIENCE("SCIENCE"),
+    SECURITY("SECURITY"), VIP("V.I.P."),
+
+    // Other skills
+    ANTHROPOLOGY("Anthropology"), ACQUISITION("Acquisition"),
+    ARCHAEOLOGY("Archaeology"), ASTROPHYSICS("Astrophysics"),
+    BARBERING("Barbering"), BIOLOGY("Biology"),
+    CANTANKEROUSNESS("Cantankerousness"), COMPUTER_SKILL("Computer Skill"),
+    CYBERNETICS("Cybernetics"), DIPLOMACY("Diplomacy"),
+    EMPATHY("Empathy"), EXOBIOLOGY("Exobiology"),
+    FCA("FCA", true),
+    GEOLOGY("Geology"), GREED("Greed"),
+    GURAMBA("Guramba"), HONOR("Honor"),
+    KLINGON_INTELLIGENCE("Klingon Intelligence", true),
+    LAW("Law"), LEADERSHIP("Leadership"),
+    MEMORY_OMEGA("Memory Omega", true),
+    MINDMELD("Mindmeld"), MIRACLE_WORKER("Miracle Worker"),
+    MUSIC("Music"), NAVIGATION("Navigation"),
+    OBSIDIAN_ORDER("Obsidian Order", true),
+    ORION_SYNDICATE("Orion Syndicate", true),
+    PHYSICS("Physics"), RESISTANCE("Resistance"),
+    SECTION_31("Section 31", true),
+    SMUGGLING("Smuggling"), STELLAR_CARTOGRAPHY("Stellar Cartography"),
+    TAL_SHIAR("Tal Shiar", true),
+    TRANSPORTER_SKILL("Transporter Skill"), TREACHERY("Treachery"),
+    V_SHAR("V'Shar", true),
+    YOUTH("Youth");
+
+    private String _humanReadable;
+    private boolean _isIntelligence;
+
+    RegularSkill(String humanReadable) {
+        this(humanReadable, false);
+    }
+
+    RegularSkill(String humanReadable, boolean isIntelligence) {
+        _humanReadable = humanReadable;
+        _isIntelligence = true;
+    }
+}
