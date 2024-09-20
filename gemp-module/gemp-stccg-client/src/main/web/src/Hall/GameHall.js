9affac8cc4cf60e5242e72cfec9198a1894b439f
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2024-09-20 13:21:58 -0500

    Move generalAdmin and leagueAdmin JS to GameHall.js.

--- gemp-module/gemp-stccg-client/src/main/web/src/Hall/GameHall.js
+++ gemp-module/gemp-stccg-client/src/main/web/src/Hall/GameHall.js
@@ -4,6 +4,7 @@ import GameHistoryUI from "../../js/gemp-022/gameHistoryUi.js";
 import PlayerStatsUI from "../../js/gemp-022/playerStatsUi.js";
 import StatsUI from "../../js/gemp-022/statsUi.js";
 import LeagueResultsUI from "../../js/gemp-022/leagueResultsUi.js";
+import { gatherData, sortOptionsByName, leagueErrorMap } from "../../js/gemp-022/leagueAdmin.js";
 import TournamentResultsUI from "../../js/gemp-022/tournamentResultsUi.js";
 import { formatPrice } from "../../js/gemp-022/common.js";
 
@@ -184,6 +185,8 @@ $(document).ready(function () {
 								},
 								hall.hallErrorMap());
 							break;
+						default:
+							console.error("helpMain.tabs(): Unknown selected tab number" + selected_help_tab_num);
 					}
 					break;
 				case 2:
@@ -225,13 +228,11 @@ $(document).ready(function () {
 						case 1:
 							// Game History
 							console.log("Game history");
-							// TODO
 							var ui = new GameHistoryUI("/gemp-stccg-server");
 							break;
 						case 2:
 							// My Stats
 							console.log("My stats");
-							// TODO
 							var ui = new PlayerStatsUI("/gemp-stccg-server");
 							break;
 						case 3:
@@ -281,10 +282,365 @@ $(document).ready(function () {
 						$("#generalTab").parent().hide();
 						$("#banTab").parent().hide();
 					}
-					
+					/*
 					if(!hall.userInfo.type.includes("l"))
 					{
 						$("#leagueTab").parent().hide();
+					}
+					*/
+
+					let selected_admin_tab = $("#adminMain").tabs("option", "active");
+					switch(selected_admin_tab) {
+						case 0:
+							// Empty landing page
+							// console.log("0");
+							break;
+						case 1:
+							// General Admin
+							$("#shutdown-button").button().click(
+								function () {
+									let execute = confirm("Are you sure you want to enter shutdown mode?	This will cancel all currently waiting tables and send a site-wide chat alert informing players the server will restart soon.	(Currently playing tables will be unaffected.)");
+									
+									if(!execute)
+										return;
+									
+									$("#shutdown-response").html("Processing...");
+										
+									hall.comm.setShutdownMode(true, function (string) {
+										$("#shutdown-response").html(string);
+									});
+								});
+							
+							
+							$("#cancel-shutdown-button").button().click(
+								function () {
+									$("#shutdown-response").html("Processing...");
+									
+									hall.comm.setShutdownMode(false, function (string) {
+										$("#shutdown-response").html(string);
+									});
+								});
+							
+							$("#clear-cache-button").button().click(
+								function () {
+									$("#cache-response").html("Processing...");
+									
+									hall.comm.clearServerCache(function (string) {
+										$("#cache-response").html(string);
+									});
+								});
+							
+							$("#reload-cards-button").button().click(
+								function () {
+									$("#cards-response").html("Processing...");
+									
+									hall.comm.reloadCardDefinitions(function (string) {
+										$("#cards-response").html(string);
+									});
+								});
+							
+							$("#motd-button").button().click(
+								function () {
+									$("#motd-response").html("Processing...");
+									
+									hall.comm.setDailyMessage($("#motd-text").val(), function (string) {
+										$("#motd-response").html("Response: " + string);
+									});
+								});
+							
+							hall.comm.getDailyMessage(function (json) {
+								$("#motd-text").val(json.response);
+								$("#motd-preview").html(json.response);
+							});
+							
+							
+							$("#add-items-button").button().click(
+								function () {
+									let execute = confirm("Are you sure you want to issue these items in these quantities to these players in that collection? THIS CANNOT BE UNDONE, EVEN MANUALLY.");
+									
+									if(!execute)
+										return;
+									
+									$("#add-items-response").html("Processing...");
+									
+									hall.comm.addItems($("#collectionType").val(), $("#product").val(), $("#players").val(), function (string) {
+										$("#add-items-response").html("Response: " + string);
+									});
+								});
+							break;
+						case 2:
+							// League admin
+							// TODO: Move all of this elsewhere and import.
+							var previewDialog = $("<div></div>").dialog({
+									autoOpen:false,
+									closeOnEscape:true,
+									resizable:true,
+									modal:true,
+									title:"Preview window",
+									closeText: ''
+								});
+
+							var displayPreview = function (xml) {
+								var root = xml.documentElement;
+								if(root == null)
+								{
+									xml = new DOMParser().parseFromString(xml,"text/xml");
+									root = xml.documentElement;
+								}
+								if (root.tagName == 'league') {
+									var league = root;
+
+									var leagueName = league.getAttribute("name");
+									var cost = parseInt(league.getAttribute("cost"));
+
+									previewDialog.append("<div class='leagueName'>" + leagueName + "</div>");
+
+									var costStr = formatPrice(cost);
+									previewDialog.append("<div class='leagueCost'><b>Cost:</b> " + costStr + "</div>");
+
+									var series = league.getElementsByTagName("series");
+									for (var j = 0; j < series.length; j++) {
+
+										var serie = series[j];
+										var serieName = serie.getAttribute("type");
+										var serieStart = serie.getAttribute("start");
+										var serieEnd = serie.getAttribute("end");
+										var maxMatches = serie.getAttribute("maxMatches");
+										var format = serie.getAttribute("format");
+										var collection = serie.getAttribute("collection");
+										var limited = serie.getAttribute("limited");
+
+										var serieText = serieName + " - " + getDateString(serieStart) + " to " + getDateString(serieEnd);
+										previewDialog.append("<div class='serieName'>" + serieText + "</div>");
+
+										previewDialog.append("<div><b>Format:</b> " + ((limited == "true") ? "Limited" : "Constructed") + " " + format + "</div>");
+										previewDialog.append("<div><b>Collection:</b> " + collection + "</div>");
+
+										previewDialog.append("<div>Maximum ranked matches in serie: " + maxMatches + "</div>");
+									}
+								}
+							};
+		
+							var now = new Date();
+							var nowStr = "" + now.getFullYear() + String(1 + now.getMonth()).padStart(2, '0') + String(now.getDate()).padStart(2, '0');
+							$("#sealed-start").val(nowStr);
+							$("#solo-draft-start").val(nowStr);
+							$("#constructed-start").val(nowStr);
+
+							var previewError = function (xhr) {
+								previewDialog.dialog("close");
+								alert("Invalid parameters specified - error code: " + xhr.status);
+							};
+		
+							$("#preview-sealed-league-button").button().click(
+								function () {
+									let resultdiv = $("#sealed-league-result");
+									resultdiv.html("Processing...");
+									
+									hall.comm.previewSealedLeague(
+										$("#sealed-format").val(), 
+										$("#sealed-start").val(), 
+										$("#sealed-duration").val(),
+										$("#sealed-matches").val(),
+										$("#sealed-name").val(),
+										$("#sealed-cost").val(), 
+										function (xml) {
+											previewDialog.html("");
+											displayPreview(xml);
+											resultdiv.html("OK");
+											previewDialog.dialog("open");
+										}, leagueErrorMap(resultdiv));
+									});
+		
+							$("#add-sealed-league-button").button().click(
+								function () {
+									let resultdiv = $("#sealed-league-result");
+									resultdiv.html("Processing...");
+									
+									hall.comm.addSealedLeague(
+										$("#sealed-format").val(), 
+										$("#sealed-start").val(), 
+										$("#sealed-duration").val(),
+										$("#sealed-matches").val(),
+										$("#sealed-name").val(),
+										$("#sealed-cost").val(), 
+										function (xml) {
+											resultdiv.html("OK");
+										}, leagueErrorMap(resultdiv));
+									});
+		
+		
+							$("#preview-solo-draft-league-button").button().click(
+								function () {
+									let resultdiv = $("#solo-draft-league-result");
+									resultdiv.html("Processing...");
+						
+									hall.comm.previewSoloDraftLeague(
+										$("#solo-draft-format").val(), 
+										$("#solo-draft-start").val(), 
+										$("#solo-draft-duration").val(),
+										$("#solo-draft-matches").val(),
+										$("#solo-draft-name").val(),
+										$("#solo-draft-cost").val(), 
+										function (xml) {
+											previewDialog.html("");
+											displayPreview(xml);
+											resultdiv.html("OK");
+											previewDialog.dialog("open");
+										}, leagueErrorMap(resultdiv));
+									});
+		
+							$("#add-solo-draft-league-button").button().click(
+								function () {
+									let resultdiv = $("#solo-draft-league-result");
+									resultdiv.html("Processing...");
+									
+									hall.comm.addSoloDraftLeague(
+										$("#solo-draft-format").val(), 
+										$("#solo-draft-start").val(), 
+										$("#solo-draft-duration").val(),
+										$("#solo-draft-matches").val(),
+										$("#solo-draft-name").val(),
+										$("#solo-draft-cost").val(), 
+										function (xml) {
+											resultdiv.html("OK");
+										}, leagueErrorMap(resultdiv));
+									});
+		
+							$("#preview-constructed-league-button").button().click(
+								function () {
+									let resultdiv = $("#constructed-league-result");
+									resultdiv.html("Processing...");
+									
+									var data = gatherData($(".series"))
+									
+									hall.comm.previewConstructedLeague(
+										$("#constructed-start").val(), 
+										$("#constructed-collection").val(),
+										$("#constructed-prize").val(),
+										$("#constructed-name").val(),
+										$("#constructed-cost").val(),
+										data.format,
+										data.seriesDuration,
+										data.maxMatches,
+										function (xml) {
+											previewDialog.html("");
+											displayPreview(xml);
+											resultdiv.html("OK");
+											previewDialog.dialog("open");
+										}, leagueErrorMap(resultdiv));
+									});
+		
+							$("#add-constructed-league-button").button().click(
+								function () {
+									let resultdiv = $("#constructed-league-result");
+									resultdiv.html("Processing...");
+									
+									var data = gatherData($(".series"))
+									
+									hall.comm.addConstructedLeague(
+										$("#constructed-start").val(), 
+										$("#constructed-collection").val(),
+										$("#constructed-prize").val(),
+										$("#constructed-name").val(),
+										$("#constructed-cost").val(),
+										data.format,
+										data.seriesDuration,
+										data.maxMatches,
+										function (xml) {
+											resultdiv.html("OK");
+										}, leagueErrorMap(resultdiv));
+									});
+
+
+							$("#previewConstructed").click(
+									function () {
+										submitFormToAddress("/gemp-stccg-server/admin/previewConstructedLeague", $("#addConstructedForm").eq(0), displayPreview, previewError);
+										previewDialog.html("");
+										previewDialog.dialog("open");
+									});
+
+							$("#add-constructed-series-button").button().click(
+									function () {
+										$(".serieData").last().clone().appendTo(".series");
+									});
+							
+							// BUG: getFormats causes a 500 server error, probably due to LOTR decks or something.
+							hall.comm.getFormats(true,
+								function (json) 
+								{
+									//console.log(json);
+									let drafts = json.DraftTemplates;
+									let formats = json.Formats;
+									let sealed = json.SealedTemplates
+									//console.log(drafts);
+									for (var prop in drafts) {
+										if (Object.prototype.hasOwnProperty.call(drafts, prop)) {
+											//console.log(prop);
+											
+											var item = $("<option/>")
+												.attr("value", prop)
+												.text(prop);
+											$("#solo-draft-format").append(item);
+										}
+									}
+									sortOptionsByName("#solo-draft-format");
+									
+									//console.log(formats);
+									for (var prop in formats) {
+										if (Object.prototype.hasOwnProperty.call(formats, prop)) {
+											//console.log(prop);
+											
+											if(formats[prop].name.includes("Limited"))
+												continue;
+											
+											let num = ("0000" + formats[prop].order).substr(-4);
+											
+											var item = $("<option/>")
+												.attr("value", prop)
+												.text("" + num + " - " + formats[prop].name);
+											$("#constructed-format").append(item);
+										}
+									}
+									sortOptionsByName("#constructed-format");
+									$("#constructed-format option").each(function(index) {
+										//console.log(this);
+										let newText = $(this).text().replace(/\d+ - /, '');
+										//console.log(newText);
+										$(this).text(newText);
+									});
+									
+									//console.log(sealed);
+									for (var prop in sealed) {
+										if (Object.prototype.hasOwnProperty.call(sealed, prop)) {
+											//console.log(prop);
+											
+											let code = sealed[prop].Format;
+											let id = sealed[prop].ID;
+											let serieCount = sealed[prop].SeriesProduct.length;
+											
+											var item = $("<option/>")
+												.attr("value", id)
+												.text(prop + " - " + serieCount + " Series");
+											$("#sealed-format").append(item);
+										}
+									}
+									sortOptionsByName("#sealed-format");
+								}, 
+								{
+									"400":function () 
+									{
+										alert("Could not retrieve formats.");
+									}
+								});
+							break;
+						case 3:
+							// Manage Users
+							console.log("Manage users");
+							break;
+						default:
+							console.error("adminMain.tabs(): Unknown selected tab number" + selected_admin_tab);
+							break;
 					}
 					break;
 				default:
