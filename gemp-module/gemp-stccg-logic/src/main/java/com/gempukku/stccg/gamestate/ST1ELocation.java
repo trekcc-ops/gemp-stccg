936a29efd94a8cc2b1286f5a0d3fa3e355eceebe
datanoh <lund_christopher@outlook.com>
2024-09-27 16:48:03 -0500

    Added logic to get far end of spaceline

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/gamestate/ST1ELocation.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/gamestate/ST1ELocation.java
@@ -105,4 +105,33 @@ public class ST1ELocation {
     public int getSpan(Player player) throws InvalidGameLogicException {
         return getMissionForPlayer(player.getPlayerId()).getSpan(player);
     }
+
+    // TODO - Really this needs to be a decision
+    public ST1ELocation getFarEndOfSpaceline(Player player) throws InvalidGameLogicException {
+        List<ST1ELocation> spaceline = _game.getGameState().getSpacelineLocations();
+        List<ST1ELocation> farthestSpan = new LinkedList<>();
+        List<ST1ELocation> farthestCards = new LinkedList<>();
+        int maxCards = 0;
+        int maxSpan = 0;
+        for (ST1ELocation location : spaceline) {
+            if (getDistanceToLocation(location, player) > maxSpan) {
+                farthestSpan = new LinkedList<>();
+                maxSpan = getDistanceToLocation(location, player);
+            }
+            if (getDistanceToLocation(location, player) >= maxSpan) {
+                farthestSpan.add(location);
+            }
+            if (Math.abs(spaceline.indexOf(location) - spaceline.indexOf(this)) > maxCards) {
+                farthestCards = new LinkedList<>();
+                maxCards = Math.abs(spaceline.indexOf(location) - spaceline.indexOf(this));
+            }
+            if (getDistanceToLocation(location, player) >= maxCards) {
+                farthestCards.add(location);
+            }
+        }
+        if (farthestCards.size() > 1)
+            return farthestSpan.getFirst();
+        else return farthestCards.getFirst();
+    }
+
 }
