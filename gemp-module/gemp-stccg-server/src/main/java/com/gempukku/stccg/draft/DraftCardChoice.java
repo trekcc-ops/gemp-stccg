9d952e9a49a3bde0d779d45487cf6a2e7303d641
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2024-10-09 12:20:34 -0500

    Removed classes not in use

--- gemp-module/gemp-stccg-server/src/main/java/com/gempukku/stccg/draft/DraftCardChoice.java
+++
@@ -1,9 +0,0 @@
-package com.gempukku.stccg.draft;
-
-import com.gempukku.stccg.collection.CardCollection;
-
-public interface DraftCardChoice {
-    long getTimeLeft();
-
-    CardCollection getCardCollection();
-}
