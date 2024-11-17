1122b49c556029d98d44bafcc8e5d943699d4bf0
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2024-11-17 12:18:33 -0600

    Updated Archer blueprint

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/cards/blueprints/Blueprint101_014.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/cards/blueprints/Blueprint101_014.java
@@ -1,56 +1,47 @@
 package com.gempukku.stccg.cards.blueprints;
 
 import com.gempukku.stccg.actions.Action;
+import com.gempukku.stccg.actions.KillSinglePersonnelAction;
+import com.gempukku.stccg.actions.choose.SelectCardInPlayAction;
+import com.gempukku.stccg.actions.discard.RemoveDilemmaFromGameAction;
 import com.gempukku.stccg.actions.missionattempt.EncounterSeedCardAction;
-import com.gempukku.stccg.actions.turn.SystemQueueAction;
+import com.gempukku.stccg.actions.missionattempt.FailDilemmaAction;
 import com.gempukku.stccg.cards.AttemptingUnit;
 import com.gempukku.stccg.cards.physicalcard.MissionCard;
-import com.gempukku.stccg.cards.physicalcard.PhysicalCard;
+import com.gempukku.stccg.cards.physicalcard.PersonnelCard;
+import com.gempukku.stccg.cards.physicalcard.ST1EPhysicalCard;
 import com.gempukku.stccg.common.filterable.SkillName;
-import com.gempukku.stccg.common.filterable.Zone;
 import com.gempukku.stccg.condition.missionrequirements.AndMissionRequirement;
 import com.gempukku.stccg.condition.missionrequirements.MissionRequirement;
+import com.gempukku.stccg.filters.Filters;
 import com.gempukku.stccg.game.DefaultGame;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 @SuppressWarnings("unused")
 public class Blueprint101_014 extends CardBlueprint {
     Blueprint101_014() {
-        super("101_012"); // Archer
+        super("101_014"); // Archer
     }
 
     @Override
-    public List<Action> getEncounterActions(PhysicalCard thisCard, DefaultGame game, AttemptingUnit attemptingUnit,
+    public List<Action> getEncounterActions(ST1EPhysicalCard thisCard, DefaultGame game, AttemptingUnit attemptingUnit,
                                             MissionCard mission, EncounterSeedCardAction action) {
         List<Action> result = new LinkedList<>();
         MissionRequirement conditions = new AndMissionRequirement(SkillName.MEDICAL, SkillName.SECURITY);
-        if (conditions.canBeMetBy(attemptingUnit.getAttemptingPersonnel())) {
-            result.add(new SystemQueueAction(game) {
-
-                @Override
-                public Action nextAction(DefaultGame cardGame) {
-                    mission.removeSeedCard(thisCard);
-                    game.getGameState().removeCardFromZone(thisCard);
-                    game.getGameState().addCardToZone(thisCard, Zone.REMOVED);
-                    return getNextAction();
-                }
-
-            });
-
-        } else {
-            // TODO
-/*            Collection<PersonnelCard> females = Filters.filter(attemptingUnit.getAttemptingPersonnel(), Filters.female);
-            Collection<PersonnelCard> highestFemales = Filters.highestTotalAttributes(females);
-            if (!highestFemales.isEmpty()) {
-                SelectCardInPlayAction selectPersonnelAction = new SelectCardInPlayAction(thisCard, thisCard.getOwner(),
-                        "Select personnel to be killed", highestFemales);
-                KillAction killAction = new KillAction(thisCard.getOwner(), thisCard, selectPersonnelAction);
-                result.add(killAction);
-            }
-            result.add(new StopAction(attemptingUnit)); */
+        if (!conditions.canBeMetBy(attemptingUnit)) {
+            Collection<PersonnelCard> highestPersonnel =
+                    Filters.highestTotalAttributes(attemptingUnit.getAttemptingPersonnel());
+            SelectCardInPlayAction selectAction =
+                    new SelectCardInPlayAction(action, thisCard.getOwner(), "Select a personnel to kill",
+                            highestPersonnel);
+            result.add(selectAction);
+            result.add(new KillSinglePersonnelAction(thisCard.getOwner(), thisCard, selectAction));
+            result.add(new FailDilemmaAction(attemptingUnit, thisCard, action));
         }
+        result.add(new RemoveDilemmaFromGameAction(attemptingUnit.getPlayer(), thisCard, mission));
         return result;
     }
 
