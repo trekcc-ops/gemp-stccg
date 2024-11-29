daa63d8338ad71bf64b8bdd0671e9f5bc11ad68b
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2024-11-29 10:30:43 -0600

    Added a test for attempting unit selection

--- gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/actions/AttemptMissionTest.java
+++ gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/actions/AttemptMissionTest.java
@@ -1,5 +1,6 @@
-package com.gempukku.stccg;
+package com.gempukku.stccg.actions;
 
+import com.gempukku.stccg.AbstractAtTest;
 import com.gempukku.stccg.cards.physicalcard.FacilityCard;
 import com.gempukku.stccg.cards.physicalcard.MissionCard;
 import com.gempukku.stccg.cards.physicalcard.PersonnelCard;
@@ -67,5 +68,59 @@ public class AttemptMissionTest extends AbstractAtTest {
         assertTrue(excavation.isCompleted());
         assertEquals(excavation.getPoints(), _game.getGameState().getPlayerScore(P1));
     }
+
+    @Test
+    public void selectAwayTeamTest() throws DecisionResultInvalidException {
+        initializeGameToTestMissionAttempt();
+
+        // Figure out which player is going first
+        assertEquals(P1, _game.getCurrentPlayerId());
+
+        autoSeedMissions();
+        while (_game.getCurrentPhase() == Phase.SEED_DILEMMA) {
+            skipDilemma();
+        }
+
+        FacilityCard outpost = null;
+        MissionCard excavation = null;
+        PersonnelCard picard = null;
+
+        for (PhysicalCard card : _game.getGameState().getAllCardsInGame()) {
+            if (Objects.equals(card.getTitle(), "Federation Outpost") && card instanceof FacilityCard facility)
+                outpost = facility;
+            if (Objects.equals(card.getTitle(), "Excavation") && card instanceof MissionCard mission)
+                excavation = mission;
+            if (Objects.equals(card.getTitle(), "Jean-Luc Picard") && card instanceof PersonnelCard personnel)
+                picard = personnel;
+        }
+
+        assertNotNull(outpost);
+        assertNotNull(excavation);
+        assertNotNull(picard);
+
+        // Seed Federation Outpost at Excavation
+        assertEquals(Phase.SEED_FACILITY, _game.getCurrentPhase());
+        seedFacility(P1, outpost, excavation.getLocation());
+        assertEquals(outpost.getLocation(), excavation.getLocation());
+        assertEquals(Phase.CARD_PLAY, _game.getCurrentPhase());
+
+        // Report Picard to outpost
+        reportCard(P1, picard, outpost);
+        assertTrue(outpost.getCrew().contains(picard));
+        skipCardPlay();
+        assertEquals(Phase.EXECUTE_ORDERS, _game.getCurrentPhase());
+
+        // Beam Picard to the planet
+        beamCard(P1, outpost, picard, excavation);
+        assertTrue(picard.getAwayTeam().isOnSurface(excavation));
+
+        // Attempt mission without specifying Away Team
+        attemptMission(P1, excavation);
+
+        // Confirm that mission was solved and player earned points
+        assertTrue(excavation.isCompleted());
+        assertEquals(excavation.getPoints(), _game.getGameState().getPlayerScore(P1));
+    }
+
 
 }
