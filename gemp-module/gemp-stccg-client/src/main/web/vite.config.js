7bca8590baad67c118f6f29e1a4b74de69115188
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2024-12-29 22:44:37 -0600

    Pull in deps from all main pages.

--- gemp-module/gemp-stccg-client/src/main/web/vite.config.js
+++ gemp-module/gemp-stccg-client/src/main/web/vite.config.js
@@ -2,9 +2,21 @@
 import babel from 'vite-babel-plugin';
 import react from '@vitejs/plugin-react';
 import inject from "@rollup/plugin-inject";
-import { defineConfig } from 'vite'
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        hall: resolve(__dirname, 'hall.html'),
+        game: resolve(__dirname, 'game.html'),
+        deckBuild: resolve(__dirname, 'deckBuild.html'),
+        soloDraft: resolve(__dirname, 'soloDraft.html')
+      },
+    },
+  },
   plugins: [
     inject({
       $: 'jquery',
