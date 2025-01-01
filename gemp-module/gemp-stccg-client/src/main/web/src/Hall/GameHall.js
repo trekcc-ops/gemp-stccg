7b65978615fd677ee4f2b202d816e3125b64d3d7
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-01-01 17:13:34 -0600

    Fix gamehall dependency on jqueryui from file.

--- gemp-module/gemp-stccg-client/src/main/web/src/Hall/GameHall.js
+++ gemp-module/gemp-stccg-client/src/main/web/src/Hall/GameHall.js
@@ -1,3 +1,15 @@
+import "jquery-ui";
+import "jquery-ui/ui/data";
+import "jquery-ui/ui/unique-id";
+import "jquery-ui/ui/disable-selection";
+import "jquery-ui/ui/plugin";
+import "jquery-ui/ui/focusable";
+import "jquery-ui/ui/tabbable";
+import "jquery-ui/ui/widgets/button";
+import "jquery-ui/ui/widgets/mouse";
+import "jquery-ui/ui/widgets/resizable";
+import "jquery-ui/ui/widgets/dialog";
+import "jquery-ui/ui/widgets/tabs";
 import ChatBoxUI from "../../js/gemp-022/chat.js";
 import GempHallUI from "../../js/gemp-022/hallUi.js";
 import GameHistoryUI from "../../js/gemp-022/gameHistoryUi.js";
@@ -260,8 +272,8 @@ $(document).ready(function () {
 									{"<>":"td","html":function() { return new Date(this.end_date).toLocaleString('sv'); }},
 								]};
 								
-									   
-								$("#tblReplays").json2html(json,template);
+								// TODO: BUG: This call will fail because we never load node-json2html.
+								// $("#tblReplays").json2html(json,template);
 							}
 							
 							
