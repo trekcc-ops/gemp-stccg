9304a3973bf42749a2e03832ca801a3afac37045
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-01-18 16:32:49 -0600

    Updated ShuffleCardsIntoDrawDeckAction class to use ActionCardResolver

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/placecard/ShuffleCardsIntoDrawDeckAction.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/placecard/ShuffleCardsIntoDrawDeckAction.java
@@ -2,6 +2,7 @@ package com.gempukku.stccg.actions.placecard;
 
 import com.gempukku.stccg.TextUtils;
 import com.gempukku.stccg.actions.Action;
+import com.gempukku.stccg.actions.ActionCardResolver;
 import com.gempukku.stccg.actions.ActionyAction;
 import com.gempukku.stccg.actions.TopLevelSelectableAction;
 import com.gempukku.stccg.cards.physicalcard.PhysicalCard;
@@ -15,24 +16,32 @@ import java.util.Collection;
 
 public class ShuffleCardsIntoDrawDeckAction extends ActionyAction implements TopLevelSelectableAction {
     private final PhysicalCard _performingCard;
-    private final Filter _cardFilter;
+    private final ActionCardResolver _cardTarget;
 
     public ShuffleCardsIntoDrawDeckAction(PhysicalCard performingCard, Player performingPlayer,
                                           Filter cardFilter) {
         super(performingPlayer, "Shuffle cards into draw deck", ActionType.PLACE_CARD);
+        _cardTarget = new ActionCardResolver(cardFilter);
         _performingCard = performingCard;
-        _cardFilter = cardFilter;
     }
 
     @Override
     public boolean requirementsAreMet(DefaultGame cardGame) {
-        Collection<PhysicalCard> cards = Filters.filter(cardGame.getGameState().getAllCardsInGame(), _cardFilter);
-        return !cards.isEmpty();
+        return !_cardTarget.willProbablyBeEmpty(cardGame);
     }
 
     @Override
     public Action nextAction(DefaultGame cardGame) throws InvalidGameLogicException {
-        Collection<PhysicalCard> cards = Filters.filter(cardGame.getGameState().getAllCardsInGame(), _cardFilter);
+        if (!_cardTarget.isResolved()) {
+            Action selectionAction = _cardTarget.getSelectionAction();
+            if (selectionAction != null && !selectionAction.wasCarriedOut()) {
+                return selectionAction;
+            } else {
+                _cardTarget.resolve(cardGame);
+            }
+        }
+
+        Collection<PhysicalCard> cards = _cardTarget.getCards(cardGame);
         cardGame.getGameState().removeCardsFromZone(_performingCard.getOwnerName(), cards);
         cardGame.getGameState().shuffleCardsIntoDeck(cards, _performingPlayerId);
         cardGame.sendMessage(TextUtils.concatenateStrings(
