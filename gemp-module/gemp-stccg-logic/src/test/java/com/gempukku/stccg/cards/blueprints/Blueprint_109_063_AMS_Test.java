2841744abab59128ab61be7b9cae538e076541d7
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-01-19 15:07:32 -0600

    AMS Fix (#142)
    * Additional testing for AMS
    
    * Added SelectValidCardCombinationFromDialogAction class
    
    * Updated DownloadMultipleCardsToSameCompatibleOutpostAction to use a SelectCardsAction
    
    * Standardized some of the members in DownloadMultipleCardsToSameCompatibleOutpostAction

--- gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/cards/blueprints/Blueprint_109_063_AMS_Test.java
+++ gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/cards/blueprints/Blueprint_109_063_AMS_Test.java
@@ -1,14 +1,17 @@
 package com.gempukku.stccg.cards.blueprints;
 
 import com.gempukku.stccg.AbstractAtTest;
+import com.gempukku.stccg.cards.physicalcard.FacilityCard;
 import com.gempukku.stccg.cards.physicalcard.PhysicalCard;
 import com.gempukku.stccg.common.DecisionResultInvalidException;
 import com.gempukku.stccg.common.filterable.Phase;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Blueprint_109_063_AMS_Test extends AbstractAtTest {
 
@@ -18,24 +21,41 @@ public class Blueprint_109_063_AMS_Test extends AbstractAtTest {
         autoSeedMissions();
 
         PhysicalCard ams = null;
-        PhysicalCard fedOutpost = null;
+        FacilityCard fedOutpost = null;
+        PhysicalCard wallace = null;
+        PhysicalCard tarses = null;
 
         for (PhysicalCard card : _game.getGameState().getAllCardsInGame()) {
             if (Objects.equals(card.getTitle(), "Assign Mission Specialists"))
                 ams = card;
             if (Objects.equals(card.getTitle(), "Federation Outpost"))
-                fedOutpost = card;
+                fedOutpost = (FacilityCard) card;
+            if (Objects.equals(card.getTitle(), "Darian Wallace"))
+                wallace = card;
+            if (Objects.equals(card.getTitle(), "Simon Tarses"))
+                tarses = card;
         }
 
         assertNotNull(ams);
         assertNotNull(fedOutpost);
+        assertNotNull(tarses);
+        assertNotNull(wallace);
         while (_game.getCurrentPhase() == Phase.SEED_DILEMMA) {
             skipDilemma();
         }
 
         seedCard(P1, fedOutpost);
         seedCard(P1, ams);
+        playerDecided(P1, "0");
         assertNotNull(_userFeedback.getAwaitingDecision(P1));
+
+        List<PhysicalCard> specialists = List.of(tarses, wallace);
+
+        selectCards(P1, specialists);
+        for (PhysicalCard specialist : specialists) {
+            assertTrue(specialist.isInPlay());
+            assertTrue(fedOutpost.getCrew().contains(specialist));
+        }
     }
 
 }
