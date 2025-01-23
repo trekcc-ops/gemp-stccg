b9f3be447d84bee4b81495a116eb3bf0842bd196
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-01-22 22:11:28 -0600

    Test CardGroup constructor, as bizarre as it is.

--- gemp-module/gemp-stccg-client/src/main/web/js/tests/jCardGroup.test.js
+++ gemp-module/gemp-stccg-client/src/main/web/js/tests/jCardGroup.test.js
@@ -1,5 +1,44 @@
-import {CardGroup, VerticalBarGroup, NormalCardGroup, PlayPileCardGroup, NormalGameCardGroup, TableCardGroup, MissionCardGroup, layoutCardElem, layoutTokens} from "../gemp-022/jCardGroup.js";
+import {describe, beforeEach, expect, test, jest} from '@jest/globals'
+import {VerticalBarGroup, NormalCardGroup, PlayPileCardGroup, NormalGameCardGroup, TableCardGroup, MissionCardGroup, layoutCardElem, layoutTokens} from "../gemp-022/jCardGroup.js";
+import CardGroup from "../gemp-022/jCardGroup.js";
 
-test('jCardGroup.js is valid syntax', () => {
-    // noop
+describe('validity', () => {
+    test('jCardGroup.js is valid syntax', () => {
+        // noop
+    });
+});
+
+describe('CardGroup', () => {
+    // BUG: This almost has to be incorrect - the code probably wants empty string if undefined.
+    test('CardGroup constructor gives undefined when id is undefined', () => {
+        document.body.innerHTML = `
+            <div id='container'/>
+        `;
+        let jqContainer = $('#container');
+        
+        // So apparently jQuery hands me all the newlines and whitespace, so that's fun.
+        let expected = `\n        <div id="undefined" class="ui-widget-content card-group"></div>`;
+        let belongTestFunc = jest.fn();
+
+        let _groupUnderTest = new CardGroup(jqContainer, belongTestFunc);
+        expect(belongTestFunc.mock.calls.length).toEqual(0); // assert not called
+        expect(jqContainer.html()).toBe(expected);
+    });
+
+    // BUG: This almost has to be incorrect - the code probably wants the string as ID if provided.
+    test('CardGroup constructor gives an empty ID when handed a string', () => {
+        document.body.innerHTML = `
+            <div id='container'/>
+        `;
+        let jqContainer = $('#container');
+        let thestring = "thestring";
+        
+        // So apparently jQuery hands me all the newlines and whitespace, so that's fun.
+        let expected = `\n        <div id="" class="ui-widget-content card-group"></div>`;
+        let belongTestFunc = jest.fn();
+
+        let _groupUnderTest = new CardGroup(jqContainer, belongTestFunc, thestring);
+        expect(belongTestFunc.mock.calls.length).toEqual(0); // assert not called
+        expect(jqContainer.html()).toBe(expected);
+    });
 });
