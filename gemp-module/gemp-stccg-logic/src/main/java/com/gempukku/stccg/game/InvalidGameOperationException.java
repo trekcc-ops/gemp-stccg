f00c58a2cf2bb60c3f8fa5526d779130795ba62e
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-01-27 11:40:39 -0600

    Added broad InvalidGameOperationException class

---
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/game/InvalidGameOperationException.java
@@ -0,0 +1,7 @@
+package com.gempukku.stccg.game;
+
+public class InvalidGameOperationException extends Exception {
+    public InvalidGameOperationException(String message) {
+        super(message);
+    }
+}
