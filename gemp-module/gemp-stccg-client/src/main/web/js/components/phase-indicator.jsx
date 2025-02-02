a62b055ea0a611fb17815c6ec69ac8d5d9aa61fa
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-01 21:47:33 -0600

    Add a phase indicator component that takes its status from GameState.

---
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/phase-indicator.jsx
@@ -0,0 +1,63 @@
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+/*
+const enum_to_friendly_text = new Map(
+    ["SEED_DOORWAY", "Seed Doorway"],
+    ["SEED_MISSION", "Seed Missions"]
+    ["SEED_DILEMMA", "Seed Dilemmas"]
+    ["SEED_FACILITY", "Seed Facility"]
+    ["BETWEEN_TURNS", "Between Turns"]
+    ["START_OF_TURN", "Start of Turn"]
+    ["CARD_PLAY", "Card Play"]
+    ["EXECUTE_ORDERS", "Execute Orders"]
+    ["END_OF_TURN", "End of Turn"]
+);
+*/
+
+const seed_steps = [
+    "SEED_DOORWAY",
+    "SEED_MISSION",
+    "SEED_DILEMMA",
+    "SEED_FACILITY"
+]
+
+const play_steps = [
+    "BETWEEN_TURNS",
+    "START_OF_TURN",
+    "CARD_PLAY",
+    "EXECUTE_ORDERS",
+    "END_OF_TURN"
+]
+
+export default function PhaseIndicator( {gamestate} ) {
+    let step_list;
+    let active_step;
+
+    if (seed_steps.indexOf(gamestate.currentPhase) != -1) {
+        step_list = seed_steps;
+        active_step = seed_steps.indexOf(gamestate.currentPhase);
+    }
+    else if (play_steps.indexOf(gamestate.currentPhase) != -1) {
+        step_list = play_steps;
+        active_step = play_steps.indexOf(gamestate.currentPhase);
+    }
+    else {
+        step_list = ["Unknown phase"];
+        active_step = 0;
+    }
+
+    return(
+        <Box sx={{ width: '100%' }}>
+            <Stepper activeStep={active_step} alternativeLabel>
+                {step_list.map((label) => (
+                <Step key={label}>
+                    <StepLabel>{label}</StepLabel>
+                </Step>
+                ))}
+            </Stepper>
+        </Box>
+    );
+}
