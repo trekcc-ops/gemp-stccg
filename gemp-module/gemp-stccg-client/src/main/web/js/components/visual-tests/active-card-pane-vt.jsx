7099d1082806968fca9df614d152a16476de0897
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-09 15:31:26 -0600

    Clarify ActiveCardPane state on VT page.

--- gemp-module/gemp-stccg-client/src/main/web/js/components/visual-tests/active-card-pane-vt.jsx
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/visual-tests/active-card-pane-vt.jsx
@@ -4,7 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import ActiveCardPane from "../active-card-pane.jsx";
 import { theme } from '../../gemp-022/common.js';
 import { ThemeProvider } from "@emotion/react";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 const root = createRoot(document.getElementById("root"));
 
@@ -25,9 +25,15 @@ root.render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Stack>
-        <ActiveCardPane />
-        <ActiveCardPane card={tomalok} /> 
+      <Stack sx={{width: "250px"}}>
+        <Stack direction={"row"}>
+            <Typography>No card selected:</Typography>
+            <ActiveCardPane />
+        </Stack>
+        <Stack direction={"row"}>
+            <Typography>Tomalok selected:</Typography>
+            <ActiveCardPane card={tomalok} /> 
+        </Stack>
       </Stack>
     </ThemeProvider>
   </StrictMode>
