790c7d50bdd411161c53347ba243d95e5131403b
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-16 13:21:54 -0600

    Add a card component that knows what stopped means.

---
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/card.jsx
@@ -0,0 +1,36 @@
+import Box from '@mui/material/Box';
+import { Badge } from '@mui/material';
+import DangerousIcon from '@mui/icons-material/Dangerous';
+
+/*
+example card: {
+  "cardId": 55,
+  "title": "Jadzia Dax",
+  "blueprintId": "112_208",
+  "owner": "andrew",
+  "locationId": 7,
+  "attachedToCardId": 48,
+  "isStopped": false,
+  "imageUrl": "https://www.trekcc.org/1e/cardimages/ds9/jadziadax.gif",
+  "cardType": "PERSONNEL",
+  "uniqueness": "UNIQUE"
+}
+*/
+
+export default function Card( {card} ) {
+    let badge_color = "error";
+    let stopped_badge = 0; // hidden by default
+    let overlay = {};
+    if (card.isStopped) {
+        stopped_badge = "Stopped";
+        overlay = {filter: "grayscale(80%)"};
+    }
+
+    return(
+        <Box sx={{height: 1, width: 1}} >
+            <Badge color={badge_color} badgeContent={stopped_badge}>
+                <img width={"100%"} src={card.imageUrl} style={overlay} />
+            </Badge>
+        </Box>
+    );
+}
