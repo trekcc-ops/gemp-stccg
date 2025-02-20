f3d4549f9867d7e71338cf05090a1b98bddd1c9f
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-19 22:49:38 -0600

    Rework PhaseIndicator to use real gamestate data vs its own stored enums.

--- gemp-module/gemp-stccg-client/src/main/web/js/components/phase-indicator.jsx
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/phase-indicator.jsx
@@ -6,51 +6,56 @@ import MobileStepper from '@mui/material/MobileStepper';
 import Stack from '@mui/material/Stack';
 import { Typography } from '@mui/material';
 
-/*
-const enum_to_friendly_text = new Map(
+const enum_to_friendly_text = new Map([
     ["SEED_DOORWAY", "Seed Doorway"],
-    ["SEED_MISSION", "Seed Missions"]
-    ["SEED_DILEMMA", "Seed Dilemmas"]
-    ["SEED_FACILITY", "Seed Facility"]
-    ["BETWEEN_TURNS", "Between Turns"]
-    ["START_OF_TURN", "Start of Turn"]
-    ["CARD_PLAY", "Card Play"]
-    ["EXECUTE_ORDERS", "Execute Orders"]
+    ["SEED_MISSION", "Seed Missions"],
+    ["SEED_DILEMMA", "Seed Dilemmas"],
+    ["SEED_FACILITY", "Seed Facility"],
+    ["BETWEEN_TURNS", "Between Turns"],
+    ["START_OF_TURN", "Start of Turn"],
+    ["CARD_PLAY", "Card Play"],
+    ["EXECUTE_ORDERS", "Execute Orders"],
     ["END_OF_TURN", "End of Turn"]
-);
-*/
+]);
 
-const seed_steps = [
-    "SEED_DOORWAY",
-    "SEED_MISSION",
-    "SEED_DILEMMA",
-    "SEED_FACILITY"
-]
-
-const play_steps = [
-    "BETWEEN_TURNS",
-    "START_OF_TURN",
-    "CARD_PLAY",
-    "EXECUTE_ORDERS",
-    "END_OF_TURN"
-]
-
-export default function PhaseIndicator( {gamestate} ) {
-    let step_list;
+function get_phase_data(gamestate) {
+    let num_steps;
     let active_step;
+    let friendly_name;
 
-    if (seed_steps.indexOf(gamestate.currentPhase) != -1) {
-        step_list = seed_steps;
-        active_step = seed_steps.indexOf(gamestate.currentPhase);
-    }
-    else if (play_steps.indexOf(gamestate.currentPhase) != -1) {
-        step_list = play_steps;
-        active_step = play_steps.indexOf(gamestate.currentPhase);
+    let phases_in_order = gamestate["phasesInOrder"];
+    let current_phase = gamestate["currentPhase"];
+
+    if (phases_in_order === undefined || current_phase === undefined) {
+        // missing data, hide UI entirely
+        num_steps = 0;
+        active_step = 0;
+        friendly_name = "";
     }
     else {
-        step_list = ["Unknown phase"];
-        active_step = 0;
+        num_steps = phases_in_order.length;
+        let step_index = phases_in_order.indexOf(current_phase);
+        if (step_index === -1) {
+            // phase-to-array mismatch, show dots but hide text
+            active_step = 0;
+            friendly_name = "";
+        }
+        else {
+            // happy path
+            active_step = step_index;
+            friendly_name = enum_to_friendly_text.get(current_phase);
+        }
     }
+
+    return {
+        "num_steps": num_steps,
+        "active_step": active_step,
+        "friendly_name": friendly_name
+    }
+}
+
+export default function PhaseIndicator( {gamestate} ) {
+    let phase_data = get_phase_data(gamestate);
 
     return(
         /*
@@ -67,11 +72,11 @@ export default function PhaseIndicator( {gamestate} ) {
         <Stack>
             <MobileStepper
                 variant="dots"
-                steps={step_list.length}
+                steps={phase_data.num_steps}
                 position="static"
-                activeStep={active_step}
+                activeStep={phase_data.active_step}
             />
-            <Typography>{step_list[active_step]}</Typography>
+            <Typography>{phase_data.friendly_name}</Typography>
         </Stack>
     );
 }
