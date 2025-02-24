c1532e4e9c54f2b74228d1ab5c388e02baa09b84
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-02-24 10:11:39 -0600

    Removed admin-only restriction from GetDailyMessageRequestHandler

--- gemp-module/gemp-stccg-server/src/main/java/com/gempukku/stccg/async/handler/hall/GetDailyMessageRequestHandler.java
+++ gemp-module/gemp-stccg-server/src/main/java/com/gempukku/stccg/async/handler/hall/GetDailyMessageRequestHandler.java
@@ -1,4 +1,4 @@
-package com.gempukku.stccg.async.handler.admin;
+package com.gempukku.stccg.async.handler.hall;
 
 import com.gempukku.stccg.async.GempHttpRequest;
 import com.gempukku.stccg.async.ServerObjects;
@@ -7,12 +7,11 @@ import com.gempukku.stccg.async.handler.ResponseWriter;
 import com.gempukku.stccg.async.handler.UriRequestHandler;
 import com.gempukku.stccg.hall.HallServer;
 
-public class GetDailyMessageRequestHandler implements UriRequestHandler, AdminRequestHandler {
+public class GetDailyMessageRequestHandler implements UriRequestHandler {
 
     @Override
     public void handleRequest(GempHttpRequest request, ResponseWriter responseWriter, ServerObjects serverObjects)
             throws Exception {
-        validateAdmin(request);
         HallServer hallServer = serverObjects.getHallServer();
         String dailyMessage = hallServer.getDailyMessage();
         if(dailyMessage != null) {
