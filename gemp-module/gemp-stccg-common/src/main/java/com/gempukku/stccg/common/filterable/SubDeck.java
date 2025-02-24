0f1167ba2e2341440a93dfdceb7081daa5672bb3
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-02-24 13:33:27 -0600

    Removed SubDeck TODO comment. No plan to implement "outside the game" and "aside" subdecks for the foreseeable future.

--- gemp-module/gemp-stccg-common/src/main/java/com/gempukku/stccg/common/filterable/SubDeck.java
+++ gemp-module/gemp-stccg-common/src/main/java/com/gempukku/stccg/common/filterable/SubDeck.java
@@ -13,11 +13,6 @@ public enum SubDeck implements Filterable {
     BATTLE_BRIDGE("tactics"),
     TRIBBLE("tribbles");
 
-    /* TODO: Implement "outside" and "aside" from Lackey? Might be nice for user experience,
-        but these really just exist in Lackey for ease of use. Gemp will be able to create "from outside the game"
-        cards on command if a legal card play, and quickly retrieve seed downloads.
-     */
-
     private final String _lackeyName;
     SubDeck(String lackeyName) {
         _lackeyName = lackeyName;
