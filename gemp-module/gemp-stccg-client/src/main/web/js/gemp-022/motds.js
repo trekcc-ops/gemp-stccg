c80c68f19f47300c46b528ddf1120afaad6db2a8
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-28 13:18:10 -0600

    Start a list of stupid MOTDs.

---
+++ gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/motds.js
@@ -0,0 +1,13 @@
+const MOTDS = [
+    "50% off sale!",
+    "Dukat did nothing wrong!",
+    "From the files of Aamin Marritza!",
+    "Half the calories of 1E Traditional!",
+    "Starring Two-Gun Tomalak!",
+    "aka Project LetVar",
+]
+
+export default function rand_motd() {
+    let rand_index = Math.floor(Math.random() * MOTDS.length);
+    return MOTDS[rand_index];
+}
