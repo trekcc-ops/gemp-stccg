42b7877900e19d30f952f37dc975153a92c5ff58
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-28 22:59:04 -0600

    Categorize the MOTDs, add a few more.

--- gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/motds.js
+++ gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/motds.js
@@ -1,11 +1,28 @@
 const MOTDS = [
+    // Sensible chuckles
     "50% off sale!",
+    "Void where prohibited.",
+
+    // Trek jokes and references
+    // TOS
+    "Fascinating.",
+    "I'm a card game, not a doctor!",
+    // TNG
+    "There are five lights!",
+    // DS9
     "Dukat did nothing wrong!",
     "From the files of Aamin Marritza!",
-    "Half the calories of 1E Traditional!",
-    "Starring Two-Gun Tomalak!",
-    "aka Project LetVar",
+    "Wait, Quark owns a bar?!?",
+    "More insidious than root beer!",
+    "Dabo!",
+    // VOY
+    "There's coffee in that nebula!",
+
+    // GEMP/TrekCC inside jokes and references
     "Dozens of players!",
+    "aka Project LetVar",
+    "Starring Two-Gun Tomalak!",
+    "Half the calories of 1E Traditional!",
 ]
 
 export default function rand_motd() {
