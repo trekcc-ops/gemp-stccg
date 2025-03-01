dde5ef79c5a692ad178c2f35b17ae7f8151cefa5
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-02-28 20:16:29 -0600

    Another silly MOTD

--- gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/motds.js
+++ gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/motds.js
@@ -5,6 +5,7 @@ const MOTDS = [
     "Half the calories of 1E Traditional!",
     "Starring Two-Gun Tomalak!",
     "aka Project LetVar",
+    "Dozens of players!",
 ]
 
 export default function rand_motd() {
