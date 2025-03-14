ca614d04c0b60ddd79bb1c98cac833b2fc10a4e3
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-03-13 23:35:36 -0500

    Switch Hand component from array lookup to map key lookup.

--- gemp-module/gemp-stccg-client/src/main/web/js/components/hand.jsx
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/hand.jsx
@@ -5,23 +5,20 @@ import Card from './card.jsx';
 function get_hand(gamestate) {
     let player_id = gamestate["requestingPlayer"];
     let player_data = gamestate["players"].filter((data) => data["playerId"] === player_id);
-    if (player_data.length != 1) {
+    if (player_data.length !== 1) {
         console.error(`player with id ${player_id} not found`);
     }
     else {
         let ids_in_hand = player_data[0]["cardGroups"]["HAND"]["cardIds"];
         // DEBUG: console.log(ids_in_hand);
         let resolved_cards = [];
+        let visible_cards_in_game = gamestate["visibleCardsInGame"];
         for (const cardid of ids_in_hand) {
-            let matching_cards = Object.values(gamestate["visibleCardsInGame"]).filter((card) => card["cardId"] === cardid);
-            if (matching_cards.length > 1) {
-                console.error(`More than 1 card matching id ${cardid}`);
-            }
-            else if (matching_cards.length === 0) {
-                console.error(`Could not find card matching id ${cardid}`);
+            if (Object.hasOwn(visible_cards_in_game, cardid)) {
+                resolved_cards.push(visible_cards_in_game[cardid]);
             }
             else {
-                resolved_cards.push(matching_cards[0]);
+                console.error(`Could not find card matching id ${cardid}`);
             }
         }
         // DEBUG: console.log(resolved_cards);
