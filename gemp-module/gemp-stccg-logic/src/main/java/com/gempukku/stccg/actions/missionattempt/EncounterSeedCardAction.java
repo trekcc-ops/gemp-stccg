5b66d701798631cbdaa100d9222c012aa06e4a78
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-06-01 14:23:22 -0500

    Don't remove a dilemma from the game if it's placed on a mission after encounter

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/missionattempt/EncounterSeedCardAction.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/missionattempt/EncounterSeedCardAction.java
@@ -53,7 +53,8 @@ public class EncounterSeedCardAction extends ActionyAction implements TopLevelSe
             setAsFailed(); */
         Action nextAction = getNextAction();
         if (nextAction == null && isInProgress()) {
-            if (_cardTarget.getCard().getGameLocation() == _missionLocation) {
+            PhysicalCard card = _cardTarget.getCard();
+            if (card.getGameLocation() == _missionLocation && !card.isPlacedOnMission()) {
                 return new RemoveDilemmaFromGameAction(cardGame.getPlayer(_performingPlayerId), _cardTarget.getCard());
             }
             setAsSuccessful();
