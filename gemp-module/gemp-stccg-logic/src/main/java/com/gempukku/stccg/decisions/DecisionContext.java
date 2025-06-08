040bcc12fd41c554c5b7dec68b1d0f2ac367189c
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-06-08 07:39:40 -0500

    Partial implementation of DecisionContext

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/decisions/DecisionContext.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/decisions/DecisionContext.java
@@ -1,6 +1,23 @@
 package com.gempukku.stccg.decisions;
 
 public enum DecisionContext {
-    SEED_MISSION_INDEX_SELECTION,
-    OTHER
+    RETURN_FIRE("Do you want to return fire?"),
+    SEED_MISSION_INDEX_SELECTION("Select location for mission"),
+    SELECT_END_OF_TURN_ACTION("Play end of turn action or pass"),
+    SELECT_MISSION_PHASE_ACTION("Play mission seed phase action"),
+    SELECT_NUMBER("Choose a number"),
+    SELECT_OPTIONAL_RESPONSE_ACTION("Optional responses"),
+    SELECT_PLAYER("Choose a player"),
+    SELECT_REQUIRED_RESPONSE_ACTION("Required responses"),
+    OTHER("");
+
+    private final String _clientText;
+
+    DecisionContext(String clientText) {
+        _clientText = clientText;
+    }
+
+    public String getClientText() {
+        return _clientText;
+    }
 }
