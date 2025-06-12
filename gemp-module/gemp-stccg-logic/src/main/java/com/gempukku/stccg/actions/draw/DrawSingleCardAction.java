7b5307f9c954d407c48db944791ca9cf0e2c5ff5
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-06-12 11:28:47 -0500

    Removing AllowResponsesAction class (#210)
    * Removed AllowResponsesAction class
    
    * Import cleanup
    
    * Import cleanup

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/draw/DrawSingleCardAction.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/actions/draw/DrawSingleCardAction.java
@@ -1,12 +1,6 @@
 package com.gempukku.stccg.actions.draw;
 
-import com.gempukku.stccg.TextUtils;
 import com.gempukku.stccg.actions.*;
-import com.gempukku.stccg.actions.turn.AllowResponsesAction;
-import com.gempukku.stccg.cards.physicalcard.PhysicalCard;
-import com.gempukku.stccg.evaluator.ConstantEvaluator;
-import com.gempukku.stccg.evaluator.Evaluator;
-import com.gempukku.stccg.evaluator.SkillDotCountEvaluator;
 import com.gempukku.stccg.game.DefaultGame;
 import com.gempukku.stccg.game.InvalidGameLogicException;
 import com.gempukku.stccg.player.Player;
