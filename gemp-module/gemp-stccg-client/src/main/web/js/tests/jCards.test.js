83be01563533a1ff4cb50c62d675564c455897bd
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-10-24 12:54:45 -0500

    Add alt tag to new test expectations.

--- gemp-module/gemp-stccg-client/src/main/web/js/tests/jCards.test.js
+++ gemp-module/gemp-stccg-client/src/main/web/js/tests/jCards.test.js
@@ -927,7 +927,7 @@ describe('jquery-card-details-dialog', () => {
 
         let cardUnderTest = new Card(blueprintId, zone, cardId, owner, imageUrl, locationIndex, upsideDown);
 
-        let expected = `<div style=\"scroll: auto\"></div><div class=\"card fullCardDivVertical\"><div class=\"three-d-card-scene\"><div class=\"three-d-card\"><div class=\"card__face card__face--back\"><img src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div><div class=\"card__face card__face--front\"><div class=\"card-load-spinner\"></div><img class=\"card_img\" style=\"width: 100%; height: 100%;\"><div class=\"borderOverlay\"><img class=\"actionArea\" src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div></div></div></div></div>`;
+        let expected = `<div style=\"scroll: auto\"></div><div class=\"card fullCardDivVertical\"><div class=\"three-d-card-scene\"><div class=\"three-d-card\"><div class=\"card__face card__face--back\"><img src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div><div class=\"card__face card__face--front\"><div class=\"card-load-spinner\"></div><img class=\"card_img\" style=\"width: 100%; height: 100%;\" alt=\"\"><div class=\"borderOverlay\"><img class=\"actionArea\" src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div></div></div></div></div>`;
 
         let container_jq = $('#container');
         
@@ -951,7 +951,7 @@ describe('jquery-card-details-dialog', () => {
         let cardUnderTest = new Card(blueprintId, zone, cardId, owner, imageUrl, locationIndex, upsideDown);
         cardUnderTest.horizontal = true;
 
-        let expected = `<div style=\"scroll: auto\"></div><div class=\"card fullCardDivHorizontal\"><div class=\"three-d-card-scene\"><div class=\"three-d-card\"><div class=\"card__face card__face--back\"><img src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div><div class=\"card__face card__face--front\"><div class=\"card-load-spinner\"></div><img class=\"card_img\" style=\"width: 100%; height: 100%;\"><div class=\"borderOverlay\"><img class=\"actionArea\" src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div></div></div></div></div>`;
+        let expected = `<div style=\"scroll: auto\"></div><div class=\"card fullCardDivHorizontal\"><div class=\"three-d-card-scene\"><div class=\"three-d-card\"><div class=\"card__face card__face--back\"><img src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div><div class=\"card__face card__face--front\"><div class=\"card-load-spinner\"></div><img class=\"card_img\" style=\"width: 100%; height: 100%;\" alt=\"\"><div class=\"borderOverlay\"><img class=\"actionArea\" src=\"test-file-stub\" style=\"width: 100%; height: 100%;\"></div></div></div></div></div>`;
 
         let container_jq = $('#container');
         
