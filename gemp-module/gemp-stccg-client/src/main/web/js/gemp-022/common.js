27391b512332f3e866719f90d07f9b94298e3a78
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2025-11-04 20:01:44 -0600

    Switch affiliation icons over to local loading since they're small.

--- gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/common.js
+++ gemp-module/gemp-stccg-client/src/main/web/js/gemp-022/common.js
@@ -2,6 +2,22 @@ import goldImg from '../../images/gold.png';
 import silverImg from '../../images/silver.png';
 import { createTheme } from '@mui/material/styles';
 import { fetchImage } from './communication.js';
+import bajAffilIcon from "../../images/icons/affiliations/1E-BAJ.gif";
+import borgAffilIcon from "../../images/icons/affiliations/1E-BORG.gif";
+import cardAffilIcon from "../../images/icons/affiliations/1E-CARD.gif";
+import domAffilIcon from "../../images/icons/affiliations/1E-DOM.gif";
+import fedAffilIcon from "../../images/icons/affiliations/1E-FED.gif";
+import ferAffilIcon from "../../images/icons/affiliations/1E-FER.gif";
+import hirAffilIcon from "../../images/icons/affiliations/1E-HIR.gif";
+import kazAffilIcon from "../../images/icons/affiliations/1E-KAZ.gif";
+import klgAffilIcon from "../../images/icons/affiliations/1E-KLG.gif";
+import neuAffilIcon from "../../images/icons/affiliations/1E-NEU.gif";
+import nonAffilIcon from "../../images/icons/affiliations/1E-NON.gif";
+import romAffilIcon from "../../images/icons/affiliations/1E-ROM.gif";
+import stfAffilIcon from "../../images/icons/affiliations/1E-STF.gif";
+import vidAffilIcon from "../../images/icons/affiliations/1E-VID.gif";
+import vulAffilIcon from "../../images/icons/affiliations/1E-VUL.gif";
+import xinAffilIcon from "../../images/icons/affiliations/1E-XIN.png";
 
 export const softwareName = "Velara"; // See TNG s01e18 "Home Soil"
 export const versionNumber = "0.0.1";
@@ -170,37 +186,37 @@ export function getAffiliationIcon(affiliationEnum) {
     // Receives the server enum name for an affiliation and provides the TrekCC icon URL
     switch(affiliationEnum) {
         case "BAJORAN":
-            return "https://www.trekcc.org/images/icons/1e/1E-BAJ.gif";
+            return bajAffilIcon;
         case "BORG":
-            return "https://www.trekcc.org/images/icons/1e/1E-BORG.gif";
+            return borgAffilIcon;
         case "CARDASSIAN":
-            return "https://www.trekcc.org/images/icons/1e/1E-CARD.gif";
+            return cardAffilIcon;
         case "DOMINION":
-            return "https://www.trekcc.org/images/icons/1e/1E-DOM.gif";
+            return domAffilIcon;
         case "FEDERATION":
-            return "https://www.trekcc.org/images/icons/1e/1E-FED.gif";
+            return fedAffilIcon;
         case "FERENGI":
-            return "https://www.trekcc.org/images/icons/1e/1E-FER.gif";
+            return ferAffilIcon;
         case "HIROGEN":
-            return "https://www.trekcc.org/images/icons/1e/1E-HIR.gif";
+            return hirAffilIcon;
         case "KAZON":
-            return "https://www.trekcc.org/images/icons/1e/1E-KAZ.gif";
+            return kazAffilIcon;
         case "KLINGON":
-            return "https://www.trekcc.org/images/icons/1e/1E-KLG.gif";
+            return klgAffilIcon;
         case "NEUTRAL":
-            return "https://www.trekcc.org/images/icons/1e/1E-NEU.gif";
+            return neuAffilIcon;
         case "NON_ALIGNED":
-            return "https://www.trekcc.org/images/icons/1e/1E-NON.gif";
+            return nonAffilIcon;
         case "ROMULAN":
-            return "https://www.trekcc.org/images/icons/1e/1E-ROM.gif";
+            return romAffilIcon;
         case "STARFLEET":
-            return "https://www.trekcc.org/images/icons/1e/1E-STF.gif";
+            return stfAffilIcon;
         case "VIDIIAN":
-            return "https://www.trekcc.org/images/icons/1e/1E-VID.gif";
+            return vidAffilIcon;
         case "VULCAN":
-            return "https://www.trekcc.org/images/icons/1e/1E-VUL.gif";
+            return vulAffilIcon;
         case "XINDI":
-            return "https://www.trekcc.org/images/icons/1e/1E-XIN.gif";
+            return xinAffilIcon;
         default:
             console.error("Cannot recognize affiliation " + affiliationEnum);
             return "";
