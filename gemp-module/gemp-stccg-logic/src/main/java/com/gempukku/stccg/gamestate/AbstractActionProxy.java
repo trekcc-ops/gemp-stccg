8f7698670f2038269c9cc28b772c926487ad8011
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-11-28 14:13:04 -0600

    Import cleanup

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/gamestate/AbstractActionProxy.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/gamestate/AbstractActionProxy.java
@@ -3,7 +3,6 @@ package com.gempukku.stccg.gamestate;
 import com.gempukku.stccg.actions.ActionResult;
 import com.gempukku.stccg.actions.TopLevelSelectableAction;
 import com.gempukku.stccg.game.DefaultGame;
-import com.gempukku.stccg.player.Player;
 
 import java.util.LinkedList;
 import java.util.List;
