2c31c4536709f6fd22d668d9d7c65eb3395ce00a
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2025-12-06 17:00:29 -0600

    Added maxTimeAllowed property to serialized PlayerClock class

--- gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/player/PlayerClock.java
+++ gemp-module/gemp-stccg-logic/src/main/java/com/gempukku/stccg/player/PlayerClock.java
@@ -1,28 +1,46 @@
 package com.gempukku.stccg.player;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.gempukku.stccg.common.GameTimer;
 
+@JsonIgnoreProperties(value = { "timeRemaining" }, allowGetters = true)
 public class PlayerClock {
-    private int _timeElapsed = 0;
+    private int _timeElapsed;
     private final String _playerId;
-    private final GameTimer _settings;
+    private final int _maxTimeAllowed;
 
-    public PlayerClock(String playerId, GameTimer settings) {
+    public PlayerClock(
+            @JsonProperty("playerId")
+            String playerId,
+            @JsonProperty("maxTimeAllowed")
+            int maxTimeAllowed,
+            @JsonProperty("timeElapsed")
+            int timeElapsed
+    ) {
+        _timeElapsed = timeElapsed;
         _playerId = playerId;
-        _settings = settings;
+        _maxTimeAllowed = maxTimeAllowed;
     }
 
-    @JsonIgnore
+    public PlayerClock(String playerId, GameTimer settings) {
+        _timeElapsed = 0;
+        _playerId = playerId;
+        _maxTimeAllowed = settings.maxSecondsPerPlayer();
+    }
+
+    @JsonProperty("timeElapsed")
     public int getTimeElapsed() {
         return _timeElapsed;
     }
 
     @JsonProperty("timeRemaining")
     private int getTimeRemainingSerialized() {
-        return _settings.maxSecondsPerPlayer() - _timeElapsed;
+        return _maxTimeAllowed - _timeElapsed;
     }
+
+    @JsonProperty("maxTimeAllowed")
+    private int getMaxTimeAllowed() { return _maxTimeAllowed; }
 
     @JsonProperty("playerId")
     public String getPlayerId() {
