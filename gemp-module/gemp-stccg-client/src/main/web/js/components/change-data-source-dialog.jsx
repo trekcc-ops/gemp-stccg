52567e2bf510ce2f9e63f38f76cd0e25c44b3286
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2026-01-06 22:55:51 -0600

    Initial pass at collecting a requested gamestate ID from the user.

--- gemp-module/gemp-stccg-client/src/main/web/js/components/change-data-source-dialog.jsx
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/change-data-source-dialog.jsx
@@ -1,6 +1,8 @@
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from '@mui/material/DialogTitle';
+import DialogActions from '@mui/material/DialogActions';
 import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import NumberField from './mui-number-field.jsx';
 
@@ -8,14 +10,30 @@ export default function ChangeDataSourceDialog({open, onCloseFunc, dataSource}) 
     function closeDataSourceDialog() {
         onCloseFunc(false);
     }
-    
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // inform the browser we're handling the event to prevent an onSubmit reload
+        const formData = new FormData(event.currentTarget);
+        const formJson = Object.fromEntries(formData.entries());
+        const gameId = formJson.gameId;
+        console.log(`Submitted game ID: ${gameId}`);
+        closeDataSourceDialog();
+    };
+
     return(
         <Dialog open={open} onClose={closeDataSourceDialog}>
             <DialogTitle>Data Source: {dataSource}</DialogTitle>
             <Stack>
                 <Typography>Set game ID:</Typography>
-                <NumberField size="small" defaultValue={0}/>
+                <form onSubmit={handleSubmit} id="set-game-id">
+                    <NumberField name="gameId" size="small" defaultValue={0}/>
+                </form>
+                
             </Stack>
+            <DialogActions>
+                <Button onClick={closeDataSourceDialog}>Cancel</Button>
+                <Button type="submit" form="set-game-id">OK</Button>
+            </DialogActions>
         </Dialog>
     )
 }
