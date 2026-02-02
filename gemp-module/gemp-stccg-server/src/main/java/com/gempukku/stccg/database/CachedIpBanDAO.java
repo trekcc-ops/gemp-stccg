17fb71de286503a90eeac013d3e8c93f12f38569
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2026-02-02 11:17:56 -0600

    Cleanup of null assignments

--- gemp-module/gemp-stccg-server/src/main/java/com/gempukku/stccg/database/CachedIpBanDAO.java
+++ gemp-module/gemp-stccg-server/src/main/java/com/gempukku/stccg/database/CachedIpBanDAO.java
@@ -3,12 +3,13 @@ package com.gempukku.stccg.database;
 import com.gempukku.stccg.async.Cached;
 import com.gempukku.stccg.async.LoggingProxy;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class CachedIpBanDAO implements IpBanDAO, Cached {
     private final IpBanDAO _delegate;
-    private Set<String> _bannedIps;
-    private Set<String> _bannedIpPrefixes;
+    private final Set<String> _bannedIps = new HashSet<>();
+    private final Set<String> _bannedIpPrefixes = new HashSet<>();
 
     public CachedIpBanDAO(DbAccess dbAccess) {
         _delegate = LoggingProxy.createLoggingProxy(IpBanDAO.class, new DbIpBanDAO(dbAccess));
@@ -16,30 +17,25 @@ public class CachedIpBanDAO implements IpBanDAO, Cached {
 
     @Override
     public final void clearCache() {
-        _bannedIps = null;
-        _bannedIpPrefixes = null;
+        _bannedIps.clear();
+        _bannedIpPrefixes.clear();
     }
 
     @Override
     public final int getItemCount() {
-        int total = 0;
-        if(_bannedIps != null)
-            total += _bannedIps.size();
-        if(_bannedIpPrefixes != null)
-            total += _bannedIpPrefixes.size();
-        return total;
+        return _bannedIps.size() + _bannedIpPrefixes.size();
     }
 
     @Override
     public final void addIpBan(String ip) {
         _delegate.addIpBan(ip);
-        _bannedIps = null;
+        _bannedIps.clear();
     }
 
     @Override
     public final void addIpPrefixBan(String ipPrefix) {
         _delegate.addIpPrefixBan(ipPrefix);
-        _bannedIpPrefixes = null;
+        _bannedIpPrefixes.clear();
     }
 
     @Override
@@ -49,23 +45,17 @@ public class CachedIpBanDAO implements IpBanDAO, Cached {
 
     @Override
     public final Set<String> getIpBans() {
-        Set<String> result = _bannedIps;
-        if (result != null)
-            return result;
-
-        result = _delegate.getIpBans();
-        _bannedIps = result;
-        return result;
+        if (_bannedIps.isEmpty()) {
+            _bannedIps.addAll(_delegate.getIpBans());
+        }
+        return _bannedIps;
     }
 
     @Override
     public final Set<String> getIpPrefixBans() {
-        Set<String> result = _bannedIpPrefixes;
-        if (result != null)
-            return result;
-
-        result = _delegate.getIpPrefixBans();
-        _bannedIpPrefixes = result;
-        return result;
+        if (_bannedIpPrefixes.isEmpty()) {
+            _bannedIpPrefixes.addAll(_delegate.getIpPrefixBans());
+        }
+        return _bannedIpPrefixes;
     }
 }
