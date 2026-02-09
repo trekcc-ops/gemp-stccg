4544f1583057d1d6e3f662bfe07a9db84a3e4872
Christopher Lund <138214984+datanoh@users.noreply.github.com>
2026-02-09 15:16:46 -0600

    Test is checking for both uniqueness & AU when downloading

--- gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/rules/CardPlayTest.java
+++ gemp-module/gemp-stccg-logic/src/test/java/com/gempukku/stccg/rules/CardPlayTest.java
@@ -2,23 +2,19 @@ package com.gempukku.stccg.rules;
 
 import com.gempukku.stccg.AbstractAtTest;
 import com.gempukku.stccg.GameTestBuilder;
-import com.gempukku.stccg.cards.cardgroup.PhysicalCardGroup;
 import com.gempukku.stccg.cards.physicalcard.FacilityCard;
 import com.gempukku.stccg.cards.physicalcard.PersonnelCard;
 import com.gempukku.stccg.cards.physicalcard.PhysicalCard;
-import com.gempukku.stccg.common.DecisionResultInvalidException;
-import com.gempukku.stccg.common.filterable.CardIcon;
 import com.gempukku.stccg.common.filterable.Phase;
-import com.gempukku.stccg.common.filterable.PropertyLogo;
-import com.gempukku.stccg.common.filterable.Zone;
 import com.gempukku.stccg.game.ST1EGame;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CardPlayAUTest extends AbstractAtTest {
+public class CardPlayTest extends AbstractAtTest {
 
     private PhysicalCard continuing;
     private PhysicalCard attention;
@@ -30,16 +26,19 @@ public class CardPlayAUTest extends AbstractAtTest {
     private PersonnelCard picard;
     private PersonnelCard lopez;
     private PhysicalCard altUnivEvent;
+    private PhysicalCard uniqueEvent;
 
     private ST1EGame initializeGame(boolean includeDownloadableCards,
                                     boolean allowAUCards) throws Exception {
         GameTestBuilder builder = new GameTestBuilder(_cardLibrary, formatLibrary, _players);
         continuing = builder.addSeedDeckCard("155_022", "Continuing Mission", P1);
         outpost = builder.addFacility("101_104", P1); // Federation Outpost
+        builder.addCardToCoreAsSeeded("991_004", "Dummy 1E Unique Warp Core Event", P1);
         if (includeDownloadableCards) {
             attention = builder.addDrawDeckCard("155_021", "Attention All Hands", P1);
             attention2 = builder.addDrawDeckCard("155_021", "Attention All Hands", P1);
             altUnivEvent = builder.addDrawDeckCard("991_003", "Dummy 1E AU Warp Core Event", P1);
+            uniqueEvent = builder.addDrawDeckCard("991_004", "Dummy 1E Unique Warp Core Event", P1);
         }
         worf = builder.addCardInHand("101_251", "Worf", P1, PersonnelCard.class);
         picard = builder.addCardInHand("101_215", "Jean-Luc Picard", P1, PersonnelCard.class);
@@ -76,60 +75,8 @@ public class CardPlayAUTest extends AbstractAtTest {
         seedCard(P1, continuing);
         useGameText(continuing, P1);
 
-        // Verify that both copies of Attention All Hands are in selectable cards, but can't select both
         assertTrue(getSelectableCards(P1).containsAll(List.of(attention, attention2)));
-        assertFalse(getSelectableCards(P1).contains(altUnivEvent));
-    }
-
-
-    public void addIconTest() throws Exception {
-        _game = initializeGame(false, false);
-        List<PhysicalCard> tngCards = List.of(worf, millin, runabout);
-        for (PhysicalCard card : tngCards) {
-            assertFalse(card.hasIcon(_game, CardIcon.TNG_ICON));
-        }
-        seedCard(P1, continuing);
-        for (PhysicalCard card : tngCards) {
-            assertTrue(card.hasIcon(_game, CardIcon.TNG_ICON));
-        }
-
-        // Does not add TNG icon to Fed Outpost
-        assertTrue(outpost.hasPropertyLogo(PropertyLogo.TNG_LOGO));
-        assertFalse(outpost.hasIcon(_game, CardIcon.TNG_ICON));
-    }
-
-    public void drawCardTest() throws Exception {
-        _game = initializeGame(false, false);
-        PhysicalCardGroup<PhysicalCard> hand = _game.getPlayer(P1).getCardGroup(Zone.HAND);
-        seedCard(P1, continuing);
-        int initialHandSize = hand.size();
-        playCard(P1, worf);
-        assertEquals(initialHandSize - 1, hand.size());
-        assertTrue(worf.isInPlay());
-        useGameText(continuing, P1);
-        assertEquals(initialHandSize, hand.size());
-    }
-
-    public void cannotDrawCardTest() throws Exception {
-        _game = initializeGame(true, false);
-        PhysicalCardGroup<PhysicalCard> hand = _game.getPlayer(P1).getCardGroup(Zone.HAND);
-        seedCard(P1, continuing);
-        useGameText(continuing, P1);
-        selectCard(P1, attention);
-        int initialHandSize = hand.size();
-
-        // Play Picard; too many skills, so can't draw a card
-        playCard(P1, picard);
-        assertEquals(initialHandSize - 1, hand.size());
-        assertTrue(picard.isInPlay());
-        assertThrows(DecisionResultInvalidException.class, () -> useGameText(continuing, P1));
-        assertEquals(initialHandSize - 1, hand.size());
-
-        // Play Lopez; has a DL icon, so can't draw a card
-        playCard(P1, lopez);
-        assertEquals(initialHandSize - 2, hand.size());
-        assertTrue(lopez.isInPlay());
-        assertThrows(DecisionResultInvalidException.class, () -> useGameText(continuing, P1));
-        assertEquals(initialHandSize - 2, hand.size());
+        assertFalse(getSelectableCards(P1).contains(altUnivEvent)); // can't download because of AU
+        assertFalse(getSelectableCards(P1).contains(uniqueEvent)); // can't download because of uniqueness
     }
 }
