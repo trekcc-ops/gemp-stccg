0c0d51f267c44c6cd695451ad8705031c88cfec9
Andrew Dorney <761994+andrewd18@users.noreply.github.com>
2026-03-09 21:13:49 -0500

    More error handling for common.jsx.

--- gemp-module/gemp-stccg-client/src/main/web/js/components/common.jsx
+++ gemp-module/gemp-stccg-client/src/main/web/js/components/common.jsx
@@ -1,14 +1,24 @@
 export function get_your_player_id(gamestate) {
-    return gamestate["requestingPlayer"];
+    if (Object.hasOwn(gamestate, "requestingPlayer")) {
+        return gamestate["requestingPlayer"];
+    }
+    else {
+        return "";
+    }
 }
 
 export function get_opponent_player_id(gamestate) {
     let your_player_id = gamestate["requestingPlayer"];
     let opponent_names = [];
-    for (const playerId of Object.keys(gamestate["playerMap"])) {
-        if (playerId != your_player_id) {
-            opponent_names.push(playerId);
+    if (Object.hasOwn(gamestate, "playerMap")) {
+        for (const playerId of Object.keys(gamestate["playerMap"])) {
+            if (playerId != your_player_id) {
+                opponent_names.push(playerId);
+            }
         }
+        return opponent_names[0]; // assume 1 opponent
     }
-    return opponent_names[0]; // assume 1 opponent
+    else {
+        return "";
+    }
 }
